278dc2c90438be34d29891f020a06adbf756a515
Wendel Lopes <wendelopes@msn.com>
2021-05-19 12:23:28 -0300

    Adding host devopement

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,8 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  # host local for api
+  config.hosts << "api.onebitfood.dev"
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
