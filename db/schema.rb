1c9e71a3ebca4914267f00acda5faf9f8e124619
Wendel Lopes <wendelopes@msn.com>
2021-05-19 18:52:51 -0300

    Adding action create for orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_163919) do
+ActiveRecord::Schema.define(version: 2021_05_19_213511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2021_05_19_163919) do
     t.bigint "restaurant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "street"
     t.index ["restaurant_id"], name: "index_orders_on_restaurant_id"
   end
 
