982b99e95f3dfb74ff212bb27f2ca76c503dbf19
Wendel Lopes <wendelopes@msn.com>
2021-05-19 11:49:30 -0300

    Created controllers

---
+++ app/controllers/api/v1/available_cities_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::AvailableCitiesController < ApplicationController
+
+  def index
+    availableCities = AvailableCities.all
+    render json: availableCities, status: :ok
+  end
+
+end
